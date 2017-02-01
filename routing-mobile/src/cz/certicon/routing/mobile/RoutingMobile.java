55c61f01b5bfe167adcf9f8302de0eb820db0389
potocek <potocek@merica.cz>
2017-02-01 13:07:05 +0100

    routing-mobile project started

---
+++ routing-mobile/src/cz/certicon/routing/mobile/RoutingMobile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.certicon.routing.mobile;
+
+/**
+ *
+ * @author Blahoslav Potoček <potocek@merica.cz>
+ */
+public class RoutingMobile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
